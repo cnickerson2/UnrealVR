a08b211623095aafdff3af79c40ec54bd7d91a50
Chris Nickerson <christophern@nait.ca>
2019-05-16 16:54:18 -0600

    Player can now teleport using controllers. Also, refactored code

--- ArchitecturalDemo/Source/ArchitecturalDemo/Character/VRCharacter.cpp
+++ ArchitecturalDemo/Source/ArchitecturalDemo/Character/VRCharacter.cpp
@@ -10,79 +10,43 @@
 #include "NavigationSystem.h"
 #include "Components/PostProcessComponent.h"
 #include "Materials/MaterialInstanceDynamic.h"
+#include "MotionControllerComponent.h"
 
-// Sets default values
 AVRCharacter::AVRCharacter()
 {
-    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
-
 
     VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
     VRRoot->SetupAttachment(GetRootComponent());
 
-
     CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
     CameraComponent->SetupAttachment(VRRoot);
 
-    DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Destination Marker"));
-    DestinationMarker->SetupAttachment(GetRootComponent());
+    LeftHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftHand"));
+    LeftHand->SetupAttachment(VRRoot);
+    LeftHand->SetTrackingSource(EControllerHand::Left);
+
+    RightHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightHand"));
+    RightHand->SetupAttachment(VRRoot);
+    RightHand->SetTrackingSource(EControllerHand::Right);
+
+    DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DestinationMarker"));
+    DestinationMarker->SetupAttachment(VRRoot);
 
     PostProcessComponent = CreateDefaultSubobject<UPostProcessComponent>(TEXT("PostProcessComponent"));
-    PostProcessComponent->SetupAttachment(GetRootComponent());
-
-
+    PostProcessComponent->SetupAttachment(VRRoot);
 }
 
-// Called when the game starts or when spawned
 void AVRCharacter::BeginPlay()
 {
     Super::BeginPlay();
 
-    DestinationMarker->SetVisibility(false); //ensure it is gone until it hits something
+    // Ensure Destination Marker isn't showing until it hits something
+    DestinationMarker->SetVisibility(false); 
 
-    UpdateBlinders();
+    SetupBlinders();
 }
 
-void AVRCharacter::UpdateBlinders()
-{
-    if (BlinderMaterialBase == nullptr)
-    {
-        UE_LOG(LogTemp, Error, TEXT("[%s] : BlinderMaterialBase was not set"), *GetName());
-        return;
-    }
-    DynamicMaterialInstance = UMaterialInstanceDynamic::Create(BlinderMaterialBase, this);
-    DynamicMaterialInstance->SetScalarParameterValue(TEXT("Radius"), 1.0);
-
-    PostProcessComponent->AddOrUpdateBlendable(DynamicMaterialInstance);
-}
-
-// Called every frame
-void AVRCharacter::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-
-    FVector NewCameraOffset = CameraComponent->GetComponentLocation() - GetActorLocation();
-    NewCameraOffset.Z = 0;
-    AddActorWorldOffset(NewCameraOffset);
-    VRRoot->AddWorldOffset(-NewCameraOffset);
-
-    UpdateDestinationMarker();
-
-    if (RadiusVsVelocity != nullptr)
-    {
-        float VelocityOfCharacter = GetVelocity().Size();
-        float Radius = RadiusVsVelocity->GetFloatValue(VelocityOfCharacter);
-
-        DynamicMaterialInstance->SetScalarParameterValue(TEXT("Radius"), Radius);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("[%s] : RadiusVsVelocity Curve Float was not set"), *GetName());
-    }
-}
-
-// Called to bind functionality to input
 void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -91,33 +55,155 @@ void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
     PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
 
-    PlayerInputComponent->BindAction(TEXT("Teleport"), IE_Released, this, &AVRCharacter::BeginTeleport);
+    PlayerInputComponent->BindAction(TEXT("TeleportRight"), IE_Released, this, &AVRCharacter::TeleportRight);
+    PlayerInputComponent->BindAction(TEXT("TeleportLeft"), IE_Released, this, &AVRCharacter::TeleportLeft);
+    PlayerInputComponent->BindAction(TEXT("TeleportCenter"), IE_Released, this, &AVRCharacter::TeleportCenter);
 }
 
-void AVRCharacter::MoveForward(float throttle)
+void AVRCharacter::Tick(float DeltaTime)
 {
-    AddMovementInput(CameraComponent->GetForwardVector()*throttle);
+    Super::Tick(DeltaTime);
+
+    CenterCharacterOnHead();
+
+    UpdateDestinationMarker();
+
+    UpdateBlinders();
 }
 
-void AVRCharacter::MoveRight(float throttle)
+void AVRCharacter::CenterCharacterOnHead()
 {
-    AddMovementInput(CameraComponent->GetRightVector()*throttle);
+    FVector NewCameraOffset = CameraComponent->GetComponentLocation() - GetActorLocation();
+    // Ensure the Offset only occurs on the X and Y axis
+    NewCameraOffset.Z = 0;
+    AddActorWorldOffset(NewCameraOffset);
+    VRRoot->AddWorldOffset(-NewCameraOffset);
+}
+
+void AVRCharacter::MoveForward(float Throttle)
+{
+    AddMovementInput(CameraComponent->GetForwardVector()*Throttle);
+}
+
+void AVRCharacter::MoveRight(float Throttle)
+{
+    AddMovementInput(CameraComponent->GetRightVector()*Throttle);
 }
 
 void AVRCharacter::UpdateDestinationMarker()
 {
     FVector TeleportLocation;
+    bool bFoundSpotOnNavMesh = CanFindTeleportDestination(TeleportLocation);
 
-    if (FindTeleportDestination(TeleportLocation))
+    if (bFoundSpotOnNavMesh)
     {
         DestinationMarker->SetWorldLocation(TeleportLocation);
-        DestinationMarker->SetVisibility(true);
-    }
-    else
-    {
-        DestinationMarker->SetVisibility(false);
     }
 
+    DestinationMarker->SetVisibility(bFoundSpotOnNavMesh);
+}
+
+bool AVRCharacter::CanFindTeleportDestination(FVector & OutLocation)
+{
+    USceneComponent* OriginComponent = nullptr;
+    GetTeleportOrigin(OriginComponent);
+    if (OriginComponent == nullptr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("[%s] : OriginComponent was null"), *GetName());
+        return false;
+    }
+  
+    FHitResult HitResult;        
+    if (!CanHitSomethingWithinTeleportDistance(HitResult, OriginComponent))
+    {
+        return false;
+    }
+
+    FNavLocation NavLocation;
+    if (!IsWhatIHitNearANavMesh(NavLocation,HitResult))
+    {
+        return false;
+    }
+
+    OutLocation = NavLocation.Location;
+
+    return true;
+}
+
+void AVRCharacter::GetTeleportOrigin(USceneComponent* &OutOriginComponent)
+{
+    switch (LastUsedHandedness)
+    {
+    case EHandedness::Left:
+        OutOriginComponent = LeftHand;
+        break;
+    case EHandedness::Right:
+        OutOriginComponent = RightHand;
+        break;
+    default:
+        OutOriginComponent = CameraComponent;
+        break;
+    }
+}
+
+bool AVRCharacter::CanHitSomethingWithinTeleportDistance(FHitResult& OutHitResult, USceneComponent* const & OriginComponent)
+{
+    uint8 AxisRotation = OriginComponent == LeftHand || OriginComponent == RightHand ? 30 : 0;
+
+    return GetWorld()->LineTraceSingleByChannel
+    (
+        OutHitResult,
+        OriginComponent->GetComponentLocation(),
+        OriginComponent->GetComponentLocation() +
+        OriginComponent->GetForwardVector().RotateAngleAxis
+        (
+            AxisRotation,
+            OriginComponent->GetRightVector()
+        ) *
+        TeleportDistance,
+        ECC_Visibility
+    );
+}
+
+bool AVRCharacter::IsWhatIHitNearANavMesh(FNavLocation& OutNavLocation, FHitResult const & HitResult)
+{
+    return UNavigationSystemV1::GetNavigationSystem(GetWorld())->ProjectPointToNavigation
+    (
+        HitResult.Location,
+        OutNavLocation,
+        TeleportProjectionExtent
+    );
+}
+
+void AVRCharacter::TeleportRight()
+{
+    if (WasThisHandTheSameAsLastTime(EHandedness::Right))
+    {
+        BeginTeleport();
+    }
+}
+
+void AVRCharacter::TeleportLeft()
+{
+    if (WasThisHandTheSameAsLastTime(EHandedness::Left))
+    {
+        BeginTeleport();
+    }
+}
+
+void AVRCharacter::TeleportCenter()
+{
+    if(WasThisHandTheSameAsLastTime(EHandedness::None))
+    {
+        BeginTeleport();
+    }
+}
+
+bool AVRCharacter::WasThisHandTheSameAsLastTime(EHandedness ThisTimesHandedness)
+{
+    bool bIsTheSame = LastUsedHandedness == ThisTimesHandedness;
+    LastUsedHandedness = ThisTimesHandedness;
+    return bIsTheSame;
 }
 
 void AVRCharacter::BeginTeleport()
@@ -130,44 +216,10 @@ void AVRCharacter::BeginTeleport()
 
 void AVRCharacter::FinishTeleport()
 {
+    // Move the Character
     SetActorLocation(DestinationMarker->GetComponentLocation() + GetCapsuleComponent()->GetScaledCapsuleHalfHeight() * GetActorUpVector());
 
     StartFade(1, 0);
-}
-
-bool AVRCharacter::FindTeleportDestination(FVector & OutLocation)
-{
-    FHitResult OutHitResult;
-    bool bHit = GetWorld()->LineTraceSingleByChannel
-    (
-        OutHitResult,
-        CameraComponent->GetComponentLocation(),
-        CameraComponent->GetComponentLocation() + CameraComponent->GetForwardVector() * TeleportDistance,
-        ECC_Visibility
-    );
-
-    if (!bHit)
-    {
-        return false;
-    }
-
-    FNavLocation NavLocation;
-
-    bool bOnNavMesh = UNavigationSystemV1::GetNavigationSystem(GetWorld())->ProjectPointToNavigation
-    (
-        OutHitResult.Location,
-        NavLocation,
-        TeleportProjectionExtent
-    );
-
-    if (!bOnNavMesh)
-    {
-        return false;
-    }
-
-    OutLocation = NavLocation.Location;
-
-    return true;
 }
 
 void AVRCharacter::StartFade(float FromAlpha, float ToAlpha)
@@ -178,4 +230,79 @@ void AVRCharacter::StartFade(float FromAlpha, float ToAlpha)
     {
         PlayerController->PlayerCameraManager->StartCameraFade(FromAlpha, ToAlpha, FadeDuration, FLinearColor::Black, false, true);
     }
+}
+
+void AVRCharacter::SetupBlinders()
+{
+    if (BlinderMaterialBase == nullptr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("[%s] : BlinderMaterialBase was not set"), *GetName());
+        return;
+    }
+    DynamicMaterialInstance = UMaterialInstanceDynamic::Create(BlinderMaterialBase, this);
+    PostProcessComponent->AddOrUpdateBlendable(DynamicMaterialInstance);
+}
+
+void AVRCharacter::UpdateBlinders()
+{
+    if (DynamicMaterialInstance == nullptr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("[%s] : DynamicMaterialInstance is null. Check to make sure BlinderMaterialBase was set"), *GetName());
+        return;
+    }
+    if (RadiusVsVelocity == nullptr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("[%s] : RadiusVsVelocity Curve Float was not set"), *GetName());
+        return;
+    }
+
+    float Speed = GetVelocity().Size();
+    float Radius = RadiusVsVelocity->GetFloatValue(Speed);
+
+    DynamicMaterialInstance->SetScalarParameterValue(TEXT("Radius"), Radius);
+
+    FVector2D Centre = GetBlinderCenter();
+    DynamicMaterialInstance->SetVectorParameterValue(TEXT("Centre"), FLinearColor(Centre.X, Centre.Y, 0));
+}
+
+FVector2D AVRCharacter::GetBlinderCenter()
+{
+    FVector CharacterVelocity = GetVelocity().GetSafeNormal();
+    if (CharacterVelocity.IsNearlyZero())
+    {
+        // Not moving, so assume center of the screen
+        return FVector2D(0.5f, 0.5f);
+    }
+
+    APlayerController* PlayerController = Cast<APlayerController>(GetController());
+    if (PlayerController == nullptr)
+    {
+        // The controller is not a player. So it's not necessary
+        return FVector2D(0.5f, 0.5f);
+    }
+    
+    FVector BlinderDirection;
+    // Get the placement of the blinder from a distance from the camera
+    const uint16 DISTANCE_FROM_CAMERA = 1000;
+    if (FVector::DotProduct(CameraComponent->GetForwardVector(), CharacterVelocity) > 0)
+    {
+        BlinderDirection = CameraComponent->GetComponentLocation() + CharacterVelocity * DISTANCE_FROM_CAMERA;
+    }
+    else
+    {
+        BlinderDirection = CameraComponent->GetComponentLocation() - CharacterVelocity * DISTANCE_FROM_CAMERA;
+    }
+
+    FVector2D ScreenLocation;
+    PlayerController->ProjectWorldLocationToScreen(BlinderDirection, ScreenLocation);
+
+    // Get the pixels of the viewport
+    int32 ViewportX, ViewportY;
+    PlayerController->GetViewportSize(ViewportX, ViewportY);
+
+    // Divide the Screen location by the viewport to get a value between 0 and 1
+    ScreenLocation.X /= ViewportX;
+    ScreenLocation.Y /= ViewportY;
+
+    return ScreenLocation;
 }
--- ArchitecturalDemo/Source/ArchitecturalDemo/Character/VRCharacter.h
+++ ArchitecturalDemo/Source/ArchitecturalDemo/Character/VRCharacter.h
@@ -6,69 +6,151 @@
 #include "GameFramework/Character.h"
 #include "VRCharacter.generated.h"
 
+
+#pragma region Forward Declarations
+class UMotionControllerComponent;
+class UInputComponent;
+class UCameraComponent;
+class USceneComponent;
+class UStaticMeshComponent;
+class UPostProcessComponent;
+class UMaterialInterface;
+class UMaterialInstanceDynamic;
+class UCurveFloat;
+#pragma endregion Forward Declarations
+
+UENUM()
+enum class EHandedness
+{
+    Left,
+    Right,
+    None
+};
+
 UCLASS()
 class ARCHITECTURALDEMO_API AVRCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
-	AVRCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-    
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+#pragma region Properties
 private:
 
-    void MoveForward(float throttle);
-    void MoveRight(float throttle);
+    /** The Parent Scene component in which all VR elements are rooted */
+    UPROPERTY(VisibleAnywhere, Category = "VR")
+    USceneComponent* VRRoot;
 
-    void UpdateDestinationMarker();
-    void BeginTeleport();
-    void FinishTeleport();
-    bool FindTeleportDestination(FVector & OutLocation);
-    void UpdateBlinders();
-
-    void StartFade(float FromAlpha, float ToAlpha);
-
-protected:
+    /** The VR Camera */
     UPROPERTY(VisibleAnywhere, Category = "Camera")
-    class UCameraComponent* CameraComponent;
-
+    UCameraComponent* CameraComponent;
+    /** The Post Processing that applies the Blinder effect */
     UPROPERTY(VisibleAnywhere, Category = "Camera")
-    class USceneComponent* VRRoot;
-
-    UPROPERTY(VisibleAnywhere, Category = "Movement")
-    class UStaticMeshComponent* DestinationMarker;
-
-    UPROPERTY(VisibleAnywhere, Category = "Camera")
-    class UPostProcessComponent * PostProcessComponent;
-
-    UPROPERTY(EditDefaultsOnly, Category = "Movement")
-    float TeleportDistance = 1000; // 10 metres?
-
-    // How long the teleport fade should last in seconds.
+    UPostProcessComponent* PostProcessComponent;
+    /** How long the teleport fade should last in seconds. */
     UPROPERTY(EditDefaultsOnly, Category = "Camera")
     float FadeDuration = 1;
+    /** The editable material in which the blinder instance is created */
+    UPROPERTY(EditDefaultsOnly, Category = "Camera")
+    UMaterialInterface * BlinderMaterialBase;
+    /** The actual material instance of the blinder that is created and updated at runtime */
+    UPROPERTY(VisibleAnywhere, Category = "Camera")
+    UMaterialInstanceDynamic * DynamicMaterialInstance;
+    /** The curve that determines how wide the blinder effect is based off velocity of the character */
+    UPROPERTY(EditDefaultsOnly, Category = "Camera")
+    UCurveFloat * RadiusVsVelocity;
 
+    /** The marker that appears on the ground */
+    UPROPERTY(VisibleAnywhere, Category = "Movement")
+    UStaticMeshComponent* DestinationMarker;
+    /** How far from the player can the marker appear */
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+    float TeleportDistance = 1000;
+    /** The distance checked by the marker to see if it can hit a nav mesh */
     UPROPERTY(EditDefaultsOnly, Category = "Movement")
     FVector TeleportProjectionExtent = FVector(100, 100, 100);
 
-    UPROPERTY(EditDefaultsOnly, Category = "Camera")
-    class UMaterialInterface * BlinderMaterialBase;
-
+    /** The Motion Controller for the left hand */
+    UPROPERTY(VisibleAnywhere, Category = "Controllers")
+    UMotionControllerComponent* LeftHand;
+    /** The Motion Controller for the right hand */
+    UPROPERTY(VisibleAnywhere, Category = "Controllers")
+    UMotionControllerComponent* RightHand;
+    /** The last hand that tried to teleport */
     UPROPERTY()
-    class UMaterialInstanceDynamic * DynamicMaterialInstance;
+    EHandedness LastUsedHandedness = EHandedness::None;
+#pragma endregion Properties
 
-    UPROPERTY(EditDefaultsOnly, Category = "Camera")
-    class UCurveFloat * RadiusVsVelocity;
+#pragma region Methods
+public:
+    /** Sets default values for this character's properties */
+    AVRCharacter();
+
+private:
+    /** Called when the game starts or when spawned */
+    void BeginPlay() override;
+    /** Called to bind functionality to input */
+    void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    /** Called every frame */
+    void Tick(float DeltaTime) override;
+
+    /** Update the offset of the camera to ensure the Character Capsule is always centered on the player's head */
+    void CenterCharacterOnHead();
+  
+    /**  Move the character forward by the throttle amount. If negative, character moves backwards */
+    void MoveForward(float Throttle);
+    /**  Move the character right by the throttle amount. If negative, character moves left */
+    void MoveRight(float Throttle);
+
+
+    /** Updates the position of the Marker */
+    void UpdateDestinationMarker();
+    /** 
+     * Try to find a destination on the NavMesh that is within Teleport Distance
+     * @param OutLocation - Assigns the Location of the Teleport Destination, if acceptable. Else, nothing is assigned.
+     * @return bool - Found an appropriate Teleport Destination on the NavMesh
+     */
+    bool CanFindTeleportDestination(FVector & OutLocation);
+    /** 
+    * Return the component that the origin of the teleport should be 
+    * @param OutOriginComponent - Assigns the Origin Component depending on the Last Used Handed
+    */
+    void GetTeleportOrigin(USceneComponent* &OutOriginComponent);
+    /** 
+     * Find out if there is something that can be hit within the Teleport Distance of the Origin
+     * @param OutHitResult - Assigns the results of hit of the line cast
+     * @param OriginComponent - The Origin Component depending on the Last Used Handed
+     * @return bool - The line cast hit something
+     */
+    bool CanHitSomethingWithinTeleportDistance(FHitResult& OutHitResult, USceneComponent* const & OriginComponent);
+    /** 
+     * Find out if the Hit Result is within the extent range of a NavMesh
+     * @param OutNavLocation - Assigns the Location on the NavMesh
+     * @param HitResult - The results of hit of the line cast
+     * @return bool - There was a NavMesh within the extent of the HitResult
+     */
+    bool IsWhatIHitNearANavMesh(FNavLocation& OutNavLocation, FHitResult const & HitResult);
+
+
+    /** Called when the right hand trigger is pulled. */
+    void TeleportRight();
+    /** Called when the left hand trigger is pulled. */
+    void TeleportLeft();
+    /** Called when teleport button is pressed by anything other than a motion controller */
+    void TeleportCenter();
+    /** Check if the player was using this hand last time they clicked teleport */
+    bool WasThisHandTheSameAsLastTime(EHandedness ThisTimesHandedness)
+    /** Start teleporting to the destination */
+    void BeginTeleport();
+    /** Finish teleporting to the destination */
+    void FinishTeleport();
+    /** Fade the Camera from one Alpha to another. 1 is fully coloured. 0 is fully clear */
+    void StartFade(float FromAlpha, float ToAlpha);
+
+
+    /** Create a dynamic Blinder material instance that can be changed by code */
+    void SetupBlinders();
+    /** Change the size of the Blinder post process based on speed */
+    void UpdateBlinders();
+    /** Return the position of the center of the blinder */
+    FVector2D GetBlinderCenter();
+#pragma endregion Methods
 };
